222e86ae4a1d703aff0b4af9575547bc4257cc88
Chaeng-Ploypaphat-S <ploypaphat.saltz@gmail.com>
2026-05-26 08:44:55 -0700

    [refactor]: rename variables to match repo's new name.

--- test/aws-cdk-rest-api.test.ts
+++ test/aws-cdk-rest-api.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib/core';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsCdkProj2 from '../lib/aws-cdk-proj2-stack';
+// import * as AwsCdkRestApi from '../lib/aws-cdk-rest-api-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-cdk-proj2-stack.ts
+// example resource in lib/aws-cdk-rest-api-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new AwsCdkProj2.AwsCdkProj2Stack(app, 'MyTestStack');
+//   const stack = new AwsCdkRestApi.AwsCdkRestApiStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
